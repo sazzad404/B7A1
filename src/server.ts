82f206ef6f9f49677887747f9dcdf7d03f20e6e9
Sazzad Hasan <sazzadhasan313@gmail.com>
2026-05-21 19:48:22 +0600

    implement users and crud operation

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,123 @@
-import express, { type Application, type Request, type Response } from "express"
-const app : Application = express()
-const port = 5000
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+import config from "./config/env";
+const app: Application = express();
+const port = config.port;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req : Request, res: Response) => {
+const pool = new Pool({
+  connectionString: config.connection_string
+  
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(30),
+            email VARCHAR(30) UNIQUE NOT NULL,
+            password VARCHAR(30) NOT NULL,
+            role VARCHAR(10) ,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+            )
+            `);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
+
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Express Server",
-    author: "Sazzad Hasan"
-  })
-})
+    author: "Sazzad Hasan",
+  });
+});
 
+app.post("/api/users", async (req: Request, res: Response) => {
+  const { name, email, password, role } = req.body;
 
-app.post("/", async(req : Request, res: Response)=>{
-const body = req.body
+  try {
+    const result = await pool.query(
+      `
+    INSERT INTO users(name, email, password, role) VALUES($1,$2,$3, $4) RETURNING name, email, role
+    `,
+      [name, email, password, role],
+    );
+    console.log(result);
 
-res.status(201).json({
-    message: "Created",
-    data:body
-})
-})
+    res.status(201).json({
+      message: "Created successfully!!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+      //   data: result.rows[0],
+    });
+  }
+});
 
+app.get("/api/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+            SELECT * FROM users
+            `);
+    res.status(200).json({
+      success: true,
+      message: "Users retrived successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+            SELECT * FROM users WHERE id=$1
+            `,
+      [id],
+    );
+
+    if(result.rows.length === 0){
+          res.status(404).json({
+      success: false,
+      message: "User not found",
+      data: {},
+    });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Users retrived successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
